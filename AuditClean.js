bd465313055e7dff0ac23ba6ea7b825eafed0bd4
Michael Rakowski <37604982+mgrak@users.noreply.github.com>
2022-04-22 11:49:22 -0700

    Update AuditClean.js
    news

--- AuditClean.js
+++ AuditClean.js
@@ -4,6 +4,20 @@ var returnTable = false; //a lot faster w/o, only need with root table ie task
 var verbose = true;
 var table = 'task';
 var maxUpdates = 5000;
+
+function updateOrigin(type) {
+    var grOrign = new GlideRecord(table);
+    if (grOrigin.get(arrayAudit[i])) {
+        grOrigin.work_notes = count + ' ' + type + ' records have been deleted due to large amount, to allow the record to load';
+        grOrigin.update();
+    }
+}
+
+function verbose(msg) {
+    if (verbose == true)
+        gs.info(msg);
+}
+
 var gr = new GlideRecord(table);
 gr.addQuery('sys_mod_count', '>=', maxUpdates);
 gr.query();
@@ -27,8 +41,7 @@ gs.info('_ 🕵️ AUDIT COUNTS ______________________');
 while (ga.next()) {
     var count = ga.getAggregate('COUNT', 'documentkey');
     if (count > maxUpdates) {
-        if (verbose)
-            gs.info(ga.tablename + ' [' + ga.documentkey + ']: ' + count);
+        verbose(ga.tablename + ' [' + ga.documentkey + ']: ' + count);
         arrayAudit.push(ga.documentkey + '');
     }
 }
@@ -49,8 +62,7 @@ gs.info('_ 💬 JOURNAL COUNTS ______________________');
 while (ga.next()) {
     var count = ga.getAggregate('COUNT', 'element_id');
     if (count > maxUpdates) {
-        if (verbose)
-            gs.info(ga.name + ' [' + ga.element_id + ']: ' + count);
+        verbose(ga.name + ' [' + ga.element_id + ']: ' + count);
         arrayJournal.push(ga.element_id + '');
     }
 }
@@ -63,19 +75,24 @@ if (keepLatest100 || deleteRecords) {
         if (keepLatest100)
             grAudit.chooseWindow(100, 100000000);
         grAudit.query();
+        var count = grAudit.getRowCount();
         if (keepLatest100) {
             var x = 0;
             while (grAudit.next()) {
                 if (x == 0)
-                    gs.info('first: ' + grAudit.sys_created_on)
+                    verbose('first: ' + grAudit.sys_created_on)
                     x++;
-                if (deleteRecords)
+                if (deleteRecords) {
                     grAudit.deleteRecord();
+                }
             }
-            gs.info('x: ' + x + ' Last: ' + grAudit.sys_created_on);
+           verbose('x: ' + x + ' Last: ' + grAudit.sys_created_on);
+            updateOrigin('Audit');
         }
-        if (deleteRecords && !keepLatest100)
+        if (deleteRecords && !keepLatest100) {
             grAudit.deleteMultiple();
+            updateOrigin('Audit');
+        }
 
         if (deleteRecords) {
             var grSet = new GlideRecord('sys_history_set');
@@ -84,26 +101,30 @@ if (keepLatest100 || deleteRecords) {
             grSet.deleteMultiple();
         }
     }
-	for (var i = 0; i < arrayJournal.length; i++) {
+    for (var i = 0; i < arrayJournal.length; i++) {
         var grJournal = new GlideRecord('sys_audit');
         grJournal.addQuery('element_id', arrayJournal[i]);
         grJournal.orderBy('sys_created_on');
         if (keepLatest100)
             grJournal.chooseWindow(100, 100000000);
         grJournal.query();
+        var count = grAudit.getRowCount();
         if (keepLatest100) {
             var x = 0;
             while (grJournal.next()) {
                 if (x == 0)
-                    gs.info('first: ' + grJournal.sys_created_on)
+                    verbose('first: ' + grJournal.sys_created_on)
                     x++;
                 if (deleteRecords)
                     grJournal.deleteRecord();
             }
-            gs.info('x: ' + x + ' Last: ' + grJournal.sys_created_on);
+            verbose('x: ' + x + ' Last: ' + grJournal.sys_created_on);
+            updateOrigin('Journal');
         }
-        if (deleteRecords && !keepLatest100)
+        if (deleteRecords && !keepLatest100) {
             grJournal.deleteMultiple();
+            updateOrigin('Journal');
+        }
 
         if (deleteRecords) {
             var grSet = new GlideRecord('sys_history_set');
